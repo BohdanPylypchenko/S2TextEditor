7961a9c4f07f679d2fce21fdc1a02b45d59c0781
Bohdan Pylypchenko <bogdan314ylypchenko@gmail.com>
2023-03-08 18:27:36 +0200

    Initial commit

---
+++ src/main/java/b314/S2TextEditor/S2TextEditorApplication.java
@@ -0,0 +1,13 @@
+package b314.S2TextEditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S2TextEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S2TextEditorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/b314/S2TextEditor/S2TextEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package b314.S2TextEditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S2TextEditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
